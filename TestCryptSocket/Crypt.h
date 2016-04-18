ecf45b3267e71be6893bbadaed72446ec96dfdff
Monkey <371816216@qq.com>
2016-04-18 22:38:45 +0800

    init

---
+++ TestCryptSocket/Crypt.h
@@ -0,0 +1,26 @@
+#ifndef _CRYPTH_
+#define _CRYPTH_
+#include <time.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <cstddef>
+class Crypt
+{
+public:
+	Crypt();
+	~Crypt();
+	static char * hashkey(const char *);
+	static void randomkey(char tmp[8]);
+	static char * desencode(const uint8_t * text,const uint8_t *key);
+	static char * desdecode(const uint8_t * text, const uint8_t * key);
+	static void hexencode(const uint8_t * text,char * out);
+	static char * hexdecode(const char * text);
+	static char * hmac64(uint32_t x[2], uint32_t y[2]);
+	static void dhexchange(char *x);
+	static void dhsecret(char *pubkey, char *prikey, char * secret);
+	static char * base64encode(const uint8_t *text);
+	static char * base64decode(const uint8_t * text);
+};
+#endif
+
