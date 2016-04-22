24ed1cf3267251f82496a6f1d11def4eecf7ca3a
ZhHong <371816216@qq.com>
2016-04-22 19:38:25 +0800

    aa

--- TestCryptSocket/main.cpp
+++ TestCryptSocket/main.cpp
@@ -14,12 +14,12 @@ using namespace std;
 
 int main(void) {
 	ODSocket odsc;
-	bool a=odsc.Connect(SERVER_ADDRESS, PORT);
+	bool a = odsc.Connect(WORK_SERVER_ADDRESS, PORT);
 	if (!a){
-		printf("connect %s:%d field!\n", SERVER_ADDRESS, PORT);
+		printf("connect %s:%d field!\n", WORK_SERVER_ADDRESS, PORT);
 	}
 	else{
-		printf("connect %s:%d success!\n", SERVER_ADDRESS, PORT);
+		printf("connect %s:%d success!\n", WORK_SERVER_ADDRESS, PORT);
 		//connect success get server chanlllage
 		char chanllage[MSGSIZE];
 		int lenr1 = odsc.Recv(chanllage, strlen(chanllage), 0);
